4fda011e1398316a075c663b9e32be3d6f56923b
Tristan <voyanui0697@gmail.com>
2017-10-19 14:36:38 +0200

    Ajout du projet

---
+++ Bato/Assets/Script/Destroy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour {
+
+
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			Destroy (this.gameObject);
+			Time.timeScale = 1;
+		}
+	}
+}
---
+++ Bato/Assets/Script/Music.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour {
+
+	public AudioSource musicSource;
+	public AudioSource musicSource2;
+	public AudioClip[] music;
+
+	private bool event2 = true;
+
+
+	public void player()
+	{
+		int rand = Random.Range (0, 2);
+		musicSource.clip = music[rand];
+		musicSource.Play();
+	}
+
+	public void mcontinue()
+	{
+		musicSource.UnPause ();
+	}
+	public void stopplayer()
+	{
+		musicSource.Pause();
+	}
+
+	public void playevent2()
+	{
+		musicSource2.clip = music [3];
+		musicSource2.Play();
+		event2 = false;
+	}
+
+	// Use this for initialization
+	void Start () {
+		player ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.Find ("Jaloux_avatar(Clone)")) 
+		{
+			if (event2 == true) {
+				stopplayer ();
+				playevent2 ();
+			}
+		}
+		if (Input.GetKey (KeyCode.Space)) {
+			mcontinue ();
+		}
+	}
+}
